977a8da8766f7956f7128763eb16e689a5992952
Lukasz Tatarynowicz <tatarynowicz92@gmail.com>
2020-04-25 13:31:34 +0200

    Fixed Broken Links. closes #45

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -4,6 +4,7 @@ import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
@@ -18,5 +19,10 @@ public class OwnerController {
     public String listOfOwner(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "/owners/index";
+    }
+
+    @RequestMapping("/find")
+    public String findOwners() {
+        return "notimplemented";
     }
 }
