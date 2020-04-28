ca857877e0df815ed45d9326715444d3f1c2cefa
Lukasz Tatarynowicz <tatarynowicz92@gmail.com>
2020-04-28 21:57:26 +0200

    added pring data jpa service for owners. closes #40

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -13,11 +13,9 @@ public class Pet extends BaseEntity{
     private String name;
     @ManyToOne
     @JoinColumn(name = "type_id")
-    @Column(name = "pet_type")
     private PetType petType;
     @ManyToOne
     @JoinColumn(name = "owner_id")
-    @Column(name = "owner")
     private Owner owner;
     @Column(name = "birth_date")
     private LocalDate birthDate;
